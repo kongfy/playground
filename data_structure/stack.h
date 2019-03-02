654a60fc5f7fab0119f1717f715f061a18206c93
景严 <jingyan.kfy@alipay.com>
2019-03-02 10:43:16 +0800

    lock-free stack

---
+++ data_structure/stack.h
@@ -0,0 +1,75 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include "common.h"
+
+#include "hazard_pointer.h"
+
+template <typename T>
+class Stack
+{
+public:
+  Stack() : top_(NULL), hazard_mgr_(1)
+  {
+  }
+
+  virtual ~Stack()
+  {
+    T tmp;
+    while (pop(tmp)) {}
+  }
+
+  void push(const T &data);
+  bool pop(T &data);
+private:
+  class qnode
+  {
+  public:
+    T data;
+    qnode *next;
+  };
+
+  qnode *top_ CACHE_ALIGNED;
+  HazardManager hazard_mgr_;
+};
+
+template <typename T>
+void Stack<T>::push(const T &data)
+{
+  qnode *t = new qnode();
+  t->data = data;
+
+  while (true) {
+    t->next = top_;
+    if (__sync_bool_compare_and_swap(&top_, t->next, t)) {
+      return;
+    }
+    asm volatile("pause\n");
+  }
+}
+
+template<typename T>
+bool Stack<T>::pop(T &data)
+{
+  HazardPointer<qnode> t(hazard_mgr_);
+
+  while (true) {
+    if (!t.acquire(&top_)) {
+      continue;
+    }
+
+    if (!t) {
+      return false;
+    }
+
+    if (__sync_bool_compare_and_swap(&top_, t, t->next)) {
+      data = t->data;
+      hazard_mgr_.retireNode(t);
+      return true;
+    }
+
+    asm volatile("pause\n");
+  }
+}
+
+#endif /* _STACK_H_ */
---
+++ data_structure/test_stack.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <algorithm>
+
+#include "common.h"
+#include "stack.h"
+
+using namespace std;
+
+static int64_t n          = 0;
+static int64_t loop_count = 0;
+
+static Stack<int64_t> stack;
+
+struct value {
+  int64_t val;
+} CACHE_ALIGNED counters[MAX_THREAD_NUM];
+
+void worker(int64_t &counter)
+{
+  int64_t t;
+  for (int64_t i = 0; i < loop_count; ++i) {
+    stack.push(i);
+    if (stack.pop(t)) {
+      counter += t;
+    }
+  }
+}
+#include <signal.h>
+#include <unistd.h>
+void segfaulthandler(int parameter)
+{
+  char gcore[50];
+  sprintf(gcore, "gcore %u", getpid());
+  system(gcore);
+  printf("done\n");
+
+  exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+  //  signal(SIGSEGV, segfaulthandler);
+
+  pthread_t threads[MAX_THREAD_NUM];
+
+  /* Check arguments to program*/
+  if(argc != 3) {
+      fprintf(stderr, "USAGE: %s <threads> <loopcount>\n", argv[0]);
+      exit(1);
+  }
+
+  /* Parse argument */
+  n          = min(atol(argv[1]), MAX_THREAD_NUM);
+  loop_count = atol(argv[2]); /* Don't bother with format checking */
+
+  memset(&counters, 0, sizeof(counters));
+
+  /* Start the threads */
+  for (int64_t i = 0; i < n; ++i) {
+    pthread_create(&threads[i], NULL, (void* (*)(void*))worker, &(counters[i].val));
+  }
+
+  for (int64_t i = 0; i < n; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+
+  int64_t counter = 0;
+  for (int64_t i = 0; i < n; ++i) {
+    printf("%ld ", counters[i].val);
+    counter += counters[i].val;
+  }
+  printf("\n");
+  printf("sum: %ld\n", counter);
+
+  if ((loop_count-1)*loop_count/2*n != counter) {
+    printf("corrupted stack, counter: %ld != total: %ld\n", counter, (loop_count-1)*loop_count/2*n);
+  } else {
+    printf("works fine!\n");
+  }
+
+
+  return 0;
+}
